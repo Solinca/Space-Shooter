ab6994344debcacb649a24e552efd38e73aa1434
Solinca <corbi.nicolas@gmail.com>
2019-08-29 21:09:27 +0900

    Init

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float velocity = 300f;
+    private new Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = transform.up * velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Bullet bullet = collision.GetComponent<Bullet>();
+
+        if (bullet == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
